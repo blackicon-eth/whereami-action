fd1ca763bbe4cc128eaac92e87e5112d7201f070
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-07-28 03:00:06 +0200

    Removed dangerous characters

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,16 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center w-screen h-screen bg-lime-100">
       <div className="flex flex-col gap-3 bg-lime-300 px-5 pb-7 pt-3 mx-2 mb-2 border-2 border-black rounded-lg shadow-[6px_6px_0px_rgba(0,0,0,1)]">
-        <label className="text-[30px] font-bold">What's the "Where am I?" composer action and how does it work?</label>
-        <p className="text-[20px] pb-5">It's easier than you think! Just follow the instructions below:</p>
+        <label className="text-[30px] font-bold">
+          What&apos;s the &quot;Where am I?&quot; composer action and how does it work?
+        </label>
+        <p className="text-[20px] pb-5">It&apos;s easier than you think! Just follow the instructions below:</p>
         <p className="text-[20px]">1. Start creating a cast in Warpcast</p>
         <p className="text-[20px]">2. Click on the composer action button</p>
-        <p className="text-[20px]">3. Select the "Where am I?" action</p>
+        <p className="text-[20px]">3. Select the &quot;Where am I?&quot; action</p>
         <p className="text-[20px]">4. Fill at least one of the fields in the form</p>
-        <p className="text-[20px]">5. Click on the "Get Coordinates" button</p>
-        <p className="text-[20px]">6. If you are satisfied with the result, click on the "Create Frame" button</p>
+        <p className="text-[20px]">5. Click on the &quot;Get Coordinates&quot; button</p>
+        <p className="text-[20px]">6. If you are satisfied with the result, click on the &quot;Create Frame&quot; button</p>
         <p className="text-[20px]">7. Done! You have successfully created a frame with your location!</p>
       </div>
     </main>
