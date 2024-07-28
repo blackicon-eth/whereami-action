14039417e61a5f260a7fb1529ba868edb09960b8
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-07-28 03:03:51 +0200

    minor modification

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { appURL } from "./utils";
 export async function generateMetadata(): Promise<Metadata> {
   return {
     title: "Where Am I?",
-    description: "A composer action to share your location!",
+    description: "A composer action to share your location on Farcaster!",
     other: {
       ...(await fetchMetadata(new URL("/frames", appURL()))),
     },
@@ -20,7 +20,7 @@ export default function Home() {
           What&apos;s the &quot;Where am I?&quot; composer action and how does it work?
         </label>
         <p className="text-[20px] pb-5">It&apos;s easier than you think! Just follow the instructions below:</p>
-        <p className="text-[20px]">1. Start creating a cast in Warpcast</p>
+        <p className="text-[20px]">1. Start by creating a cast in Warpcast</p>
         <p className="text-[20px]">2. Click on the composer action button</p>
         <p className="text-[20px]">3. Select the &quot;Where am I?&quot; action</p>
         <p className="text-[20px]">4. Fill at least one of the fields in the form</p>
