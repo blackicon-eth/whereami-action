ab02efe064e19c7e112c55f757c52a45e4c6dbfe
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-07-28 03:55:22 +0200

    temporary solution

--- app/actions/create/route.tsx
+++ app/actions/create/route.tsx
@@ -2,6 +2,43 @@ import { appURL } from "../../utils";
 import { frames } from "../../frames/frames";
 import { composerAction, composerActionForm, error } from "frames.js/core";
 
+function isComposerActionState(value: any) {
+  return (
+    typeof value === "object" &&
+    !!value &&
+    "text" in value &&
+    typeof value.text === "string" &&
+    "embeds" in value &&
+    Array.isArray(value.embeds) &&
+    value.embeds.every((embed: any) => typeof embed === "string" && !!embed)
+  );
+}
+function extractComposerActionStateFromMessage(ctx: any) {
+  if (
+    "message" in ctx &&
+    typeof ctx.message === "object" &&
+    !!ctx.message &&
+    "state" in ctx.message &&
+    !!ctx.message.state
+  ) {
+    try {
+      if (typeof ctx.message.state !== "string") {
+        throw new Error("State is not a string");
+      }
+      const jsonString = decodeURIComponent(ctx.message.state);
+      const parsedState = JSON.parse(jsonString);
+      if (isComposerActionState(parsedState.cast)) {
+        return parsedState;
+      }
+      throw new Error("Invalid state shape");
+    } catch (e) {
+      console.warn(`warpcastComposerActionState: Could not parse composer action state.`);
+      return void 0;
+    }
+  }
+  return void 0;
+}
+
 export const GET = async () => {
   return composerAction({
     action: {
@@ -25,12 +62,13 @@ export const POST = frames(async (ctx) => {
     return error("Must be authenticated");
   }
 
-  // in case of composer actions we can't use ctx.state because the composer actions
-  if (!ctx.composerActionState) {
+  // Check if the state is valid
+  const composerActionState = extractComposerActionStateFromMessage(ctx);
+  if (!composerActionState) {
     return error("Must be called from composer");
   }
 
-  createActionUrl.searchParams.set("state", JSON.stringify(ctx.composerActionState));
+  createActionUrl.searchParams.set("state", JSON.stringify(composerActionState.cast));
 
   return composerActionForm({
     title: "Pick your location",
