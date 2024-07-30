d63f97d91e304680fe2807375202cf11d3d9169c
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-07-30 22:41:46 +0200

    Added metadata and logo

--- app/actions/create/route.tsx
+++ app/actions/create/route.tsx
@@ -2,16 +2,32 @@ import { appURL } from "../../utils";
 import { frames } from "../../frames/frames";
 import { composerAction, composerActionForm, error } from "frames.js/core";
 
+type ComposerActionMetadata = {
+  type: "composer"; // Must be 'composer'
+  name: string; // Maximum 15 characters
+  icon: string; // Valid Octicon. See cast actions spec.
+  description: string; // Maximum 20 characters
+  imageUrl: string; // Remote image URL
+  aboutUrl: string; // Optional "about" page URL
+  action: {
+    type: "post"; // Same as cast actions spec
+  };
+};
+
+const metadata: ComposerActionMetadata = {
+  type: "composer",
+  name: "Where Am I?",
+  icon: "pin",
+  description: "Share your location",
+  imageUrl: `${appURL()}/logo-small.png`,
+  aboutUrl: `${appURL()}`,
+  action: {
+    type: "post",
+  },
+};
+
 export const GET = async () => {
-  return composerAction({
-    action: {
-      type: "post",
-    },
-    icon: "pin",
-    name: "Where Am I?",
-    aboutUrl: `${appURL()}`,
-    description: "Share your location",
-  });
+  return composerAction(metadata);
 };
 
 export const POST = frames(async (ctx) => {
